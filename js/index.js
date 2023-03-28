d18198fa9e7ebd0757bc9dd0f0a91a874fafc719
miksezea <mikk289e@edu.zealand.dk>
2023-03-28 12:47:16 +0200

    getall og totalprice

--- js/index.js
+++ js/index.js
@@ -1,10 +1,44 @@
+const baseUrl = "https://shoppingapimikse.azurewebsites.net/api/shoppingitems"
+
 Vue.createApp({
     data() {
         return {
-
+            shoppingitems: [],
+            addData: { name: "", price: 0, quantity: 0 },
+            totalValue: 0,
+            deleteId: 0,
         }
     },
     methods: {
-
+        getAllShoppingItems() {
+            this.helperGetAndShow(baseUrl)
+        },
+        getTotalPrice() {
+            const url = baseUrl + "/" + "Sum"
+            totalValue = this.helperGetAndShow(url)
+        },
+        async helperGetAndShow(url) {
+            try {
+                const response = await axios.get(url, config)
+                this.shoppingitems = await response.data
+            } catch (ex) {
+                alert(ex.message)
+            }
+        },
+        async deleteShoppingItem(deleteId) {
+            const url = baseUrl + "/" + deleteId
+            try {
+                response = await axios.delete(url)
+            } catch (ex) {
+                alert(ex.message)
+            }
+        },
+        async addShoppingItem() {
+            try {
+                response = await axios.post(baseUrl, this.addData)
+            } catch (ex) {
+                alert(ex.message)
+            }
+        }
     }
 }).mount("#app")
